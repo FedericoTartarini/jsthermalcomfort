be00ebc1e454d619ca9f346e52d062b0b094d5a5
Christin <rche0014@uni.sydney.edu.au>
2023-09-02 20:44:16 +1000

    finish t_mrt

--- src/psychrometrics/index.js
+++ src/psychrometrics/index.js
@@ -4,5 +4,6 @@ import { t_o } from "./t_o";
 import { enthalpy } from "./enthalpy";
 import { t_wb } from "./t_wb";
 import { t_dp } from "./t_dp";
+import { t_mrt } from "./t_mrt";
 
-export default { p_sat, p_sat_torr, t_o, enthalpy, t_wb, t_dp };
+export default { p_sat, p_sat_torr, t_o, enthalpy, t_wb, t_dp, t_mrt };

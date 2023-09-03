b76fcc31fbf7ffaeb36e7206e8f3bbfff6f259f2
Jordan Ocokoljic <jordan.ocokoljic@chilled.vc>
2023-09-04 01:16:59 +1000

    Add `net` function, and associated docs and tests

--- src/models/index.js
+++ src/models/index.js
@@ -1,5 +1,6 @@
 import { heat_index } from "./heat_index";
 import { phs } from "./phs";
 import { humidex } from "./humidex";
+import { net } from "./net";
 
-export default { heat_index, phs, humidex };
+export default { heat_index, phs, humidex, net };

91d3ba339feee121577fbe827db748399d923ffa
Christin <rche0014@uni.sydney.edu.au>
2023-09-05 20:03:47 +1000

    resolve comments

--- src/psychrometrics/t_mrt.js
+++ src/psychrometrics/t_mrt.js
@@ -13,6 +13,8 @@ const o = 0.0000000567;
 
 /**
  * Converts globe temperature reading into mean radiant temperature in accordance with either the Mixed Convection developed by Teitelbaum E. et al. (2022) or the ISO 7726:1998 Standard [5]_.
+ * @see {@link t_mrt_array} for a version that supports arrays
+ * 
  * @param {number} tg - globe temperature, [°C]
  * @param {number} tdb - air temperature, [°C]
  * @param {number} v - air speed, [m/s]
@@ -40,26 +42,29 @@ export function t_mrt(
   let tr = 0;
   if (standard === "mixed convection") {
     tr = get_tr_mixed_convection(tg, tdb, v, d, emissivity);
-    const d_valid = valid_range([d], [0.04, 0.15]);
-    return isNaN(d_valid[0]) ? NaN : round(tr, 1);
+    return d >= 0.04 && d <= 0.15 ? round(tr, 1) : NaN;
   }
 
   if (standard === "iso") {
     return get_tr_iso(tg, tdb, v, d, emissivity);
   }
+  throw new Error(
+    "No standard found, please choose standard from ISO and Mixed Convectioin",
+  );
 }
 
 /**
  * Converts globe temperature reading into mean radiant temperature in accordance with either the Mixed Convection developed by Teitelbaum E. et al. (2022) or the ISO 7726:1998 Standard [5]_.
+ * Accepts array arguments. @see {@link t_mrt} for scalar arguments
+ *
  * @param {number[]} tg - globe temperature, [°C]
  * @param {number[]} tdb - air temperature, [°C]
  * @param {number[]} v - air speed, [m/s]
  * @param {number[]} d - diameter of the globe, [m] default 0.15 m
  * @param {number[]} emissivity - emissivity of the globe temperature sensor, default 0.95
-* @param {"Mixed Convection" | "ISO"} standard - either choose between the Mixed Convection and ISO formulations.
-refers to documentation of t_mrt function
-* @returns {number[]}
-*/
+ * @param {"Mixed Convection" | "ISO"} standard - either choose between the Mixed Convection and ISO formulations. Refers to t_mrt function
+ * @returns {number[]}
+ */
 
 export function t_mrt_array(
   tg,
@@ -89,6 +94,9 @@ export function t_mrt_array(
     });
     return tr_array;
   }
+  throw new Error(
+    "No standard found, please choose standard from ISO and Mixed Convectioin",
+  );
 }
 
 function get_tr_mixed_convection(_tg, _tdb, _v, _d, _emissivity) {

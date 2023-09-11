e56eedce7875c4da16e6122136060fa432cfbf6a
Yuchen <yumu8107@uni.sydney.edu.au>
2023-09-11 18:40:29 +1000

    set_tmp new API done

--- src/models/set_tmp.js
+++ src/models/set_tmp.js
@@ -5,7 +5,12 @@ import {
   check_standard_compliance_array,
   round,
 } from "../utilities/utilities.js";
-import { two_nodes, two_nodes_array } from "../models/two_nodes.js";
+import {
+  two_nodes,
+  two_nodes_array,
+  fill_array,
+  roundArray,
+} from "../models/two_nodes.js";
 
 /**
  * @typedef {Object} SetTmpKwargs
@@ -27,6 +32,10 @@ import { two_nodes, two_nodes_array } from "../models/two_nodes.js";
  * clothing, standardized for the activity concerned is the same as
  * that from a person in the actual environment with actual clothing
  * and activity level.
+ * @public
+ * @memberof models
+ *
+ * @see {@link set_tmp_array} for a version that supports arrays
  *
  * @param {number} tdb Dry bulb air temperature, default in [°C] in [°F] if `units` = 'IP'.
  * @param {number} tr Mean radiant temperature, default in [°C]
@@ -34,12 +43,12 @@ import { two_nodes, two_nodes_array } from "../models/two_nodes.js";
  * @param {number} rh Relative humidity, [%].
  * @param {number} met Metabolic rate, [W/(m2)]
  * @param {number} clo Clothing insulation, [clo]
- * @param {number} wme External work, [W/(m2)] default 0
- * @param {number} body_surface_area Body surface area, default value 1.8258 [m2] in [ft2] if units = ‘IP’
- * @param {number} p_atm Atmospheric pressure, default value 101325 [Pa] in [atm] if units = ‘IP’
- * @param {"standing" | "sitting"} body_position Select either “sitting” or “standing”
- * @param {"SI" | "IP"} units Select the SI (International System of Units) or the IP (Imperial Units) system.
- * @param {boolean} limit_inputs By default, if the inputs are outsude the following limits the function returns nan. If False returns values regardless of the input values.
+ * @param {number} [wme=0] External work, [W/(m2)] default 0
+ * @param {number} [body_surface_area=1.8258] Body surface area, default value 1.8258 [m2] in [ft2] if units = ‘IP’
+ * @param {number} [p_atm=101325] Atmospheric pressure, default value 101325 [Pa] in [atm] if units = ‘IP’
+ * @param {"standing" | "sitting"} [body_position="standing"] Select either “sitting” or “standing”
+ * @param {"SI" | "IP"} [units="SI"] Select the SI (International System of Units) or the IP (Imperial Units) system.
+ * @param {boolean} [limit_inputs=true] By default, if the inputs are outsude the following limits the function returns nan. If False returns values regardless of the input values.
  * @param {SetTmpKwargs} [kwargs]
  * @returns {number} SET – Standard effective temperature in array, [°C]
  *
@@ -132,30 +141,26 @@ export function set_tmp(
   }
   return set_tmp;
 }
-// console.log(check_standard_compliance("ashrae", {tdb:25, tr:25,v:0.1, met:1.2, clo:0.5}))
-console.log(
-  set_tmp(77, 77, 0.328, 50, 1.2, 0.5, 0, 1.8258, 101325, "sitting", "SI"),
-);
 
 /**
- * @typedef {Object} SetTmpKwargs
+ * @typedef {Object} SetTmpArrayKwargs
  * @property {boolean} [round=true]
  * @property {boolean} [calculate_ce=false]
+ * @public
  */
 
 /**
- * @typedef {Object} SetTmpKwargsRequired
+ * @typedef {Object} SetTmpArrayKwargsRequired
  * @property {boolean} round
  * @property {boolean} calculate_ce
  */
 /**
- * Calculates the Standard Effective Temperature (SET). The SET is the
- * temperature of a hypothetical isothermal environment at 50% (rh),
- * <0.1 m/s (20 fpm) average air speed (v), and tr = tdb, in which the
- * total heat loss from the skin of an imaginary occupant wearing
- * clothing, standardized for the activity concerned is the same as
- * that from a person in the actual environment with actual clothing
- * and activity level.
+ *
+ * Calculates the SET when the input parameters are arrays
+ * @public
+ * @memberof models
+ *
+ * @see {@link set_tmp} for a version that supports scalar arguments
  *
  * @param {number[]} tdbArray Dry bulb air temperature, default in [°C] in [°F] if `units` = 'IP'.
  * @param {number[]} trArray Mean radiant temperature, default in [°C]
@@ -163,9 +168,9 @@ console.log(
  * @param {number[]} rhArray Relative humidity, [%].
  * @param {number[]} metArray Metabolic rate, [W/(m2)]
  * @param {number[]} cloArray Clothing insulation, [clo]
- * @param {number[]} wmeArray External work, [W/(m2)] default 0
- * @param {number[]} bodySurfaceArray Body surface area, default value 1.8258 [m2] in [ft2] if units = ‘IP’
- * @param {number[]} pAtmArray Atmospheric pressure, default value 101325 [Pa] in [atm] if units = ‘IP’
+ * @param {number[]} [wme=[0]] External work, [W/(m2)] default 0
+ * @param {number} [body_surface_area=1.8258] Body surface area, default value 1.8258 [m2] in [ft2] if units = ‘IP’
+ * @param {number} [p_atm=101325] Atmospheric pressure, default value 101325 [Pa] in [atm] if units = ‘IP’
  * @param {"standing" | "sitting"} bodyPositionArray Select either “sitting” or “standing”
  * @param {"SI" | "IP"} units Select the SI (International System of Units) or the IP (Imperial Units) system.
  * @param {boolean} limit_inputs By default, if the inputs are outsude the following limits the function returns nan. If False returns values regardless of the input values.
@@ -173,19 +178,19 @@ console.log(
  * @returns {number[]} SET Array – Standard effective temperature in array, [°C]
  *
  * @example
- * const set = set_tmp(25, 25, 0.1, 50, 1.2, 0.5); // returns 24.3
+ * const set = set_tmp_array([25, 25], [25, 25], [0.1, 0.1], [50, 50], [1.2, 1.2], [0.5, 0.5]); // returns [24.3, 24.3]
  */
 export function set_tmp_array(
-  tdb,
-  tr,
-  v,
-  rh,
-  met,
-  clo,
-  wme = 0,
+  tdbArray,
+  trArray,
+  vArray,
+  rhArray,
+  metArray,
+  cloArray,
+  wme = [0],
   body_surface_area = 1.8258,
   p_atm = 101325,
-  body_position = "standing",
+  body_position = ["standing"],
   units = "SI",
   limit_inputs = true,
   kwargs = {},
@@ -197,6 +202,11 @@ export function set_tmp_array(
 
   let joint_kwargs = Object.assign(defaults_kwargs, kwargs);
 
+  const wmeArray = fill_array(wme, tdbArray);
+  const bodySurfaceArray = fill_array([body_surface_area], tdbArray);
+  const pAtmArray = fill_array([p_atm], tdbArray);
+  const bodyPositionArray = fill_array(body_position, tdbArray);
+
   if (units === "IP") {
     if (body_surface_area === 1.8258) {
       body_surface_area = 19.65;
@@ -204,51 +214,47 @@ export function set_tmp_array(
     if (p_atm === 101325) {
       p_atm = 1;
     }
-    const unit_convert = units_converter({
-      tdb,
-      tr,
-      v,
-      area: body_surface_area,
-      pressure: p_atm,
-    });
-    tdb = unit_convert.tdb;
-    tr = unit_convert.tr;
-    v = unit_convert.v;
+    const unit_convert = units_converter_array(
+      {
+        tdb: tdbArray,
+        tr: trArray,
+        v: vArray,
+        area: bodySurfaceArray,
+        pressure: pAtmArray,
+      },
+      "IP",
+    );
+    tdbArray = unit_convert.tdb;
+    trArray = unit_convert.tr;
+    vArray = unit_convert.v;
     body_surface_area = unit_convert.area;
     p_atm = unit_convert.pressure;
   }
 
-  const tdbArray = Array.isArray(tdb) ? tdb : [tdb];
-  const trArray = Array.isArray(tr) ? tr : [tr];
-  const vArray = Array.isArray(v) ? v : [v];
-  const rhArray = Array.isArray(rh) ? rh : [rh];
-  const metArray = Array.isArray(met) ? met : [met];
-  const cloArray = Array.isArray(clo) ? clo : [clo];
-  const wmeArray = Array.isArray(wme) ? wme : [wme];
-
-  let setArray = two_nodes(
-    (tdb = tdbArray),
-    (tr = trArray),
-    (v = vArray),
-    (rh = rhArray),
-    (met = metArray),
-    (clo = cloArray),
-    (wme = wmeArray),
-    (body_surface_area = body_surface_area),
-    (p_atmospheric = p_atm),
-    (body_position = body_position),
-    (joint_kwargs.calculate_ce = false),
-    (joint_kwargs.round = false),
+  let setArray = two_nodes_array(
+    tdbArray,
+    trArray,
+    vArray,
+    rhArray,
+    metArray,
+    cloArray,
+    wmeArray,
+    bodySurfaceArray,
+    pAtmArray,
+    bodyPositionArray,
+    [90],
+    { round: false },
   ).set;
-  //   console.log(setArray);
 
   if (units === "IP") {
-    const convertedSetArray = units_converter(setArray, "SI");
-    setArray = convertedSetArray[0];
+    const convertedSet = units_converter_array(
+      {
+        tmp: setArray,
+      },
+      "SI",
+    );
+    setArray = convertedSet.tmp;
   }
-
-  // setArray = Array.isArray(setArray) ? setArray : [setArray];
-  // console.log(setArray);
 
   if (limit_inputs) {
     const {
@@ -257,7 +263,13 @@ export function set_tmp_array(
       v: vValid,
       met: metValid,
       clo: cloValid,
-    } = check_standard_compliance_array("ashrae", { tdb, tr, v, met, clo });
+    } = check_standard_compliance_array("ashrae", {
+      tdb: tdbArray,
+      tr: trArray,
+      v: vArray,
+      met: metArray,
+      clo: cloArray,
+    });
 
     const allValid = !(
       tdbValid.includes(NaN) ||
@@ -266,14 +278,25 @@ export function set_tmp_array(
       metValid.includes(NaN) ||
       cloValid.includes(NaN)
     );
-
-    // setArray = setArray.map((value, index) => {
-    //   allValid[index] ? value : NaN;
-    // });
+    if (allValid) {
+      setArray = setArray;
+    } else {
+      for (let index = 0; index < tdbArray.length; index++) {
+        if (
+          isNaN(tdbValid[index]) ||
+          isNaN(trValid[index]) ||
+          isNaN(vValid[index]) ||
+          isNaN(metValid[index]) ||
+          isNaN(cloValid[index])
+        ) {
+          setArray[index] = NaN;
+        }
+      }
+    }
   }
 
   if (joint_kwargs.round) {
-    return round(setArray, 1);
+    return roundArray(setArray, 1);
   }
   return setArray;
 }

11f8f87ff924d7e03034427af2d1597c628c7e30
Christin <rche0014@uni.sydney.edu.au>
2023-10-01 22:17:29 +1100

    utci finished

--- tests/models/utci.test.js
+++ tests/models/utci.test.js
@@ -1,5 +1,8 @@
 import { expect, describe, it } from "@jest/globals";
-import { deep_close_to_array, deep_close_to_obj_arrays } from "../test_utilities";
+import {
+  deep_close_to_array,
+  deep_close_to_obj_arrays,
+} from "../test_utilities";
 import { utci, utci_array } from "../../src/models/utci.js";
 
 describe("utci", () => {
@@ -59,47 +62,53 @@ describe("utci", () => {
       const result = utci(tdb, tr, v, rh, units, return_stress_category);
       if (return_stress_category) {
         for (let [key, value] of Object.entries(result)) {
-            expect(value).toBe(expected[key]);
-          }
+          expect(value).toBe(expected[key]);
+        }
       } else expect(result).toBe(expected);
     },
   );
 });
 
-describe('utci_array', () => { 
-    it.each([
-        {tdb: [25, 19, 19, 27, 27, 27, 51, 27],
-        tr: [27, 24, 14, 22, 22, 22, 22, 22],
-        v: [1,1,1,1,10, 16, 16, 0],
-        rh:[50, 50, 50, 50, 50, 50, 50, 50],
-        expected: [25.2, 20.0, 16.8, 25.5, 20, 15.8, NaN, NaN]}
-    ])(
-        "Returns $expected when tdb is $tdb, tr is $tr, rh is $rh, v is $v",
-        ({tdb, tr, v,rh, expected}) => {
-            const result = utci_array(tdb, tr,v,rh)
-            deep_close_to_array(result, expected, 0)
-        }
-    )
+describe("utci_array", () => {
+  it.each([
+    {
+      tdb: [25, 19, 19, 27, 27, 27, 51, 27],
+      tr: [27, 24, 14, 22, 22, 22, 22, 22],
+      v: [1, 1, 1, 1, 10, 16, 16, 0],
+      rh: [50, 50, 50, 50, 50, 50, 50, 50],
+      expected: [25.2, 20.0, 16.8, 25.5, 20, 15.8, NaN, NaN],
+    },
+  ])(
+    "Returns $expected when tdb is $tdb, tr is $tr, rh is $rh, v is $v",
+    ({ tdb, tr, v, rh, expected }) => {
+      const result = utci_array(tdb, tr, v, rh);
+      deep_close_to_array(result, expected, 0);
+    },
+  );
 
-    it.each([{
-        tdb: [25, 25],
-        tr:[27, 25],
-        v:[1, 1],
-        rh:[50, 50],
-        units: "si",
-        expected: {
-            utci: [25.2, 24.6],
-            stress_category: ["no thermal stress", "no thermal stress"]
-        }
-    }])(
-        "Returns $expected when tdb is $tdb, tr is $tr, rh is $rh, v is $v",
-        ({tdb, tr, v,rh, units, expected}) => {
-            const result = utci_array(tdb, tr,v,rh, units, true)
-            deep_close_to_array(result.utci, expected.utci, 0)
-            expect(result.stress_category.length).toBe(expected.stress_category.length)
-            for(let i = 0; i < result.stress_category.length; i++) {
-                expect(result.stress_category[i]).toBe(expected.stress_category[i])
-            }
-        }
-    )
- })
+  it.each([
+    {
+      tdb: [25, 25],
+      tr: [27, 25],
+      v: [1, 1],
+      rh: [50, 50],
+      units: "si",
+      expected: {
+        utci: [25.2, 24.6],
+        stress_category: ["no thermal stress", "no thermal stress"],
+      },
+    },
+  ])(
+    "Returns $expected when tdb is $tdb, tr is $tr, rh is $rh, v is $v",
+    ({ tdb, tr, v, rh, units, expected }) => {
+      const result = utci_array(tdb, tr, v, rh, units, true);
+      deep_close_to_array(result.utci, expected.utci, 0);
+      expect(result.stress_category.length).toBe(
+        expected.stress_category.length,
+      );
+      for (let i = 0; i < result.stress_category.length; i++) {
+        expect(result.stress_category[i]).toBe(expected.stress_category[i]);
+      }
+    },
+  );
+});

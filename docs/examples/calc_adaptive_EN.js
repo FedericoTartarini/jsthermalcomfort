49d24186e0dc5217bb71bb7c9ea793890535920e
Jingyu Lu <jilu2919@uni.sydney.edu.au>
2023-10-10 02:07:14 +1100

    finished calc_adaptive_EN.js

---
+++ docs/examples/calc_adaptive_EN.js
@@ -0,0 +1,33 @@
+import {
+  adaptive_en,
+  adaptive_en_array,
+} from "../../src/models/adaptive_en.js";
+import { running_mean_outdoor_temperature } from "../../src/utilities/utilities.js";
+
+const result_adaptive_en = adaptive_en(25, 25, 24, 0.1);
+console.log(result_adaptive_en);
+
+const result_adaptive_en_array = adaptive_en_array(
+  [22.5, 25],
+  [22.5, 25],
+  [24, 24],
+  [0.1, 0.1],
+);
+console.log(result_adaptive_en_array);
+
+console.log(result_adaptive_en_array.tmp_cmf);
+
+const result_adaptive_en_ip = adaptive_en(72.5, 72.5, 75, 0.1, "IP");
+console.log(result_adaptive_en_ip);
+
+const rmt_value = running_mean_outdoor_temperature(
+  [29, 28, 30, 29, 28, 30, 27],
+  0.9,
+);
+const result_adaptive_en_based_on_rmt_value = adaptive_en(
+  25,
+  25,
+  rmt_value,
+  0.3,
+);
+console.log(result_adaptive_en_based_on_rmt_value);

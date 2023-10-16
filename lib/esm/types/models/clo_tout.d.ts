f6189cc87c369cd1ac5feea548754a4f8063ed6f
Jingyu Lu <jilu2919@uni.sydney.edu.au>
2023-10-16 20:58:58 +1100

    npm run build

---
+++ lib/esm/types/models/clo_tout.d.ts
@@ -0,0 +1,45 @@
+/**
+ * Representative clothing insulation Icl as a function of outdoor air
+ * temperature at 06:00 a.m {@link #ref_4|[4]}.
+ *
+ * Note: The ASHRAE 55 2020 states that it is acceptable to determine the
+ * clothing insulation Icl using this equation in mechanically conditioned
+ * buildings {@link #ref_1|[1]}.
+ *
+ * @public
+ * @memberof models
+ * @docname Clothing prediction
+ *
+ * @see {@link clo_tout_array} for a version that supports arrays
+ *
+ * @param {number} tout - outdoor air temperature at 06:00 a.m., default in
+ * [°C] in [°F] if `units` = 'IP'
+ * @param {("IP" | "SI")} units - select the SI (International System of Units)
+ * or the IP (Imperial Units) system.
+ *
+ * @returns {number} Representative clothing insulation Icl, [clo]
+ */
+export function clo_tout(tout: number, units?: ("IP" | "SI")): number;
+/**
+ * Representative clothing insulation Icl as a function of outdoor air
+ * temperature at 06:00 a.m {@link #ref_4|[4]}.
+ *
+ * Note: The ASHRAE 55 2020 states that it is acceptable to determine the
+ * clothing insulation Icl using this equation in mechanically conditioned
+ * buildings {@link #ref_1|[1]}.
+ *
+ * @public
+ * @memberof models
+ * @docname Clothing prediction (array version)
+ *
+ * @see {@link clo_tout} for a version that supports scalar arguments
+ *
+ * @param {number[]} tout - outdoor air temperatures at 06:00 a.m., default in
+ * [°C] in [°F] if `units` = 'IP'
+ * @param {("IP" | "SI")} units  - select the SI (International System of Units)
+ * or the IP (Imperial Units) system.
+ *
+ * @returns {number[]} Representative clothing insulation Icl, [clo]
+ */
+export function clo_tout_array(tout: number[], units?: ("IP" | "SI")): number[];
+//# sourceMappingURL=clo_tout.d.ts.map

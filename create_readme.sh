45f47924eed4769bf0e360b8f34bebabd2fde57b
Santiago Zaldivar <santiago.zaldivar.1997@gmail.com>
2023-10-28 14:56:00 +1100

    autogenerate readme

---
+++ create_readme.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+printf "# Overview\n\n" > README.md
+cat ./docs_theme/overview.md >> README.md
+printf "\n\n# Installation\n\n" >> README.md
+cat ./docs_theme/installation.md >> README.md
+printf "\n\n# Examples and Tutorials\n\n" >> README.md
+cat ./docs_theme/examples.md >> README.md
+printf "\n\n# Contributing\n\n" >> README.md
+cat ./docs_theme/contributing.md >> README.md

38918510337bdef2af660b3f9d1a778c33118a65
royce-chen <chenqizi@gmail.com>
2025-03-14 06:03:49 +1100

    separated each test cases and handled array outputs

--- tests/models/a_pmv.test.js
+++ tests/models/a_pmv.test.js
@@ -1,39 +1,41 @@
-import { beforeAll, describe, expect, it } from "@jest/globals";
+import { describe, expect, test } from "@jest/globals";
 import { a_pmv } from "../../src/models/a_pmv.js";
 import { testDataUrls } from "./comftest";
-import { loadTestData, shouldSkipTest } from "./testUtils"; // Use the utils
+import { loadTestData } from "./testUtils"; // Use the utils
 
-let testData;
-let tolerance;
-
-beforeAll(async () => {
-  const result = await loadTestData(testDataUrls.aPmv, "a_pmv");
-  testData = result.testData;
-  tolerance = result.tolerance;
-});
+// use top-level await to load test data before tests are defined.
+const { testData, tolerance } = await loadTestData(testDataUrls.aPmv, "a_pmv");
 
 describe("a_pmv", () => {
-  it("Runs the test after data is loaded and skips data containing arrays", () => {
-    testData.data.forEach((testCase) => {
+  test.each(testData.data.map((testCase, index) => [index, testCase]))(
+    "Test case #%d",
+    (index, testCase) => {
       const { inputs, outputs } = testCase;
       const expected = outputs.a_pmv;
-      console.log("Inputs: ", inputs);
-      console.log("Expected: ", expected);
-
-      if (shouldSkipTest(inputs)) {
-        console.log("Skipping test with array data");
-        return; // skip the test
-      }
+      console.log(`Running test case #${index + 1}:`, inputs, expected);
 
       const { tdb, tr, vr, rh, met, clo, a_coefficient, wme } = inputs;
       const result = a_pmv(tdb, tr, vr, rh, met, clo, a_coefficient, wme);
 
-      if (isNaN(result) || expected === null) {
-        expect(result).toBeNaN();
+      // if expected is an array, then result should be an array
+      if (Array.isArray(expected)) {
+        expect(Array.isArray(result)).toBe(true);
+        // compare elements in the array
+        result.forEach((element, index) => {
+          if (isNaN(element) || expected[index] === null) {
+            expect(element).toBeNaN();
+          } else {
+            expect(element).toBeCloseTo(expected[index], tolerance);
+          }
+        });
       } else {
-        expect(result).toBeCloseTo(expected, tolerance);
-        console.log("actually close!");
+        expect(Array.isArray(result)).toBe(false);
+        if (isNaN(result) || expected === null) {
+          expect(result).toBeNaN();
+        } else {
+          expect(result).toBeCloseTo(expected, tolerance);
+        }
       }
-    });
-  });
+    },
+  );
 });

c45ed5b19db13cb76c071c614c3caa6d42506513
royce-chen <chenqizi@gmail.com>
2025-03-27 19:01:18 +1100

    refactor: enhance a_pmv test to handle array inputs and improve result validation

--- tests/models/a_pmv.test.js
+++ tests/models/a_pmv.test.js
@@ -1,27 +1,34 @@
 import { describe, test } from "@jest/globals";
-import { a_pmv } from "../../src/models/a_pmv.js";
+import { a_pmv, a_pmv_array } from "../../src/models/a_pmv.js";
 import { testDataUrls } from "./comftest";
 import { loadTestData, validateResult } from "./testUtils"; // Use the utils
 
 let output_variable = "a_pmv";
 
 // use top-level await to load test data before tests are defined.
-const { testData, tolerance } = await loadTestData(
+let { testData, tolerance } = await loadTestData(
   testDataUrls.aPmv,
   output_variable,
+  false,
 );
 
 describe(output_variable, () => {
-  test.each(testData.data.map((testCase, index) => [index, testCase]))(
-    "Test case #%d",
-    (_, testCase) => {
-      const { inputs, outputs } = testCase;
-      const expected = outputs.a_pmv;
+  // automatically number each test case
+  test.each(testData.data)("Test case #%#", (testCase) => {
+    const { inputs, outputs } = testCase;
+    const expectedOutput = outputs.a_pmv;
+    const { tdb, tr, vr, rh, met, clo, a_coefficient, wme } = inputs;
 
-      const { tdb, tr, vr, rh, met, clo, a_coefficient, wme } = inputs;
-      const result = a_pmv(tdb, tr, vr, rh, met, clo, a_coefficient, wme);
+    // Determine if any input is an array
+    const hasArrayInput = Object.values(inputs).some((value) =>
+      Array.isArray(value),
+    );
 
-      validateResult(result, expected, tolerance, { inputs, expected });
-    },
-  );
+    // Choose the appropriate function based on whether any inputs are arrays
+    const modelResult = hasArrayInput
+      ? a_pmv_array(tdb, tr, vr, rh, met, clo, a_coefficient, wme)
+      : a_pmv(tdb, tr, vr, rh, met, clo, a_coefficient, wme);
+
+    validateResult(modelResult, expectedOutput, tolerance, inputs);
+  });
 });

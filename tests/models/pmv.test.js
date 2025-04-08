664f1f786661acbf3869f740cbd694012ddac34f
royce-chen <chenqizi@gmail.com>
2025-04-08 19:19:24 +1000

    refactored pmv test and only include one sample case now

--- tests/models/pmv.test.js
+++ tests/models/pmv.test.js
@@ -1,31 +1,31 @@
-import { expect, describe, it, beforeAll } from "@jest/globals";
-import { loadTestData, shouldSkipTest } from "./testUtils"; // Use modular utilities to load data and skip array checks
+import { describe } from "@jest/globals";
 import { pmv } from "../../src/models/pmv.js";
-import { testDataUrls } from "./comftest";
-
-let testData;
-let tolerance;
-
-beforeAll(async () => {
-  const result = await loadTestData(testDataUrls.pmv, "pmv"); // Load remote data and extract tolerance
-  testData = result.testData;
-  tolerance = result.tolerance;
-});
+import { validateResult } from "./testUtils.js";
 
 describe("pmv", () => {
-  it("should run tests after data is loaded and skip data containing arrays", () => {
-    if (!testData || !testData.data)
-      throw new Error("Data not loaded or undefined");
+  test("Test case with default parameters", () => {
+    const tolerances = {
+      pmv: 0.1,
+      ppd: 1,
+    };
 
-    testData.data.forEach(({ inputs, outputs }) => {
-      // Use shouldSkipTest to check and skip data containing arrays
-      if (shouldSkipTest(inputs) || outputs === undefined) return;
+    const inputs = {
+      tdb: 22,
+      tr: 22,
+      rh: 60,
+      vr: 0.1,
+      met: 1.2,
+      clo: 0.5,
+      standard: "ISO",
+    };
 
-      const { tdb, tr, vr, rh, met, clo, standard } = inputs;
-      const result = pmv(tdb, tr, vr, rh, met, clo, undefined, standard);
+    const outputs = {
+      pmv: -0.8,
+    };
 
-      // Compare calculated result with expected output
-      expect(result).toBeCloseTo(outputs.pmv, tolerance);
-    });
+    const { tdb, tr, vr, rh, met, clo, wme, standard } = inputs;
+    const modelResult = { pmv: pmv(tdb, tr, vr, rh, met, clo, wme, standard) };
+
+    validateResult(modelResult, outputs, tolerances, inputs);
   });
 });
